bd190d32d0f92c0f7a5a432194015c7796386bdf
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-06-23 08:08:49 +0300

    fix 7 lab

--- lab7/SQLQuery1.sql
+++ lab7/SQLQuery1.sql
@@ -10,7 +10,6 @@ LEFT JOIN subject AS sub ON sub.id_subject = l.id_subject
 LEFT JOIN mark AS m ON m.id_lesson = l.id_lesson
 LEFT JOIN student AS st ON st.id_student = m.id_student
 WHERE sub.name = 'информатика'
-
 GROUP BY st.id_student, st.name
 GO
 
@@ -21,44 +20,39 @@ SELECT * FROM informatics_grades;
 -- предмета. Должниками считаются студенты, не имеющие оценки по предмету,
 -- который ведется в группе. Оформить в виде процедуры, на входе
 -- идентификатор группы.
-CREATE PROCEDURE students_with_study_debts
+CREATE PROCEDURE students_with_study_debts 
 	@id_group AS INT
-	AS
-	SELECT *
-	FROM mark AS m
-	FULL JOIN lesson AS l ON l.id_lesson = m.id_lesson
-	FULL JOIN student AS s ON s.id_group = l.id_group
-	WHERE s.id_group = 3
+AS
+	SELECT std.name, subj.name
+	FROM (
+		SELECT st.id_student, s.id_subject, COUNT(mark) AS counter, g.id_group
+		FROM [group] AS g
+		LEFT JOIN student AS st ON st.id_group = g.id_group
+		LEFT JOIN lesson AS l ON l.id_group = g.id_group
+		LEFT JOIN mark AS m ON m.id_student = st.id_student AND m.id_lesson = l.id_lesson
+		LEFT JOIN subject AS s ON s.id_subject = l.id_subject
+		GROUP BY st.id_student, s.id_subject, g.id_group
+		HAVING COUNT(mark) = 0) AS debets
+	LEFT JOIN student AS std ON std.id_student = debets.id_student
+	LEFT JOIN subject AS subj ON subj.id_subject = debets.id_subject
+	WHERE debets.id_group = @id_group
+GO
 
-
-
-
-SELECT std.name, subj.name
-FROM (
-	SELECT st.id_student, s.id_subject, COUNT(mark) AS counter
-	FROM [group] AS g
-	LEFT JOIN student AS st ON st.id_group = g.id_group
-	LEFT JOIN lesson AS l ON l.id_group = g.id_group
-	LEFT JOIN mark AS m ON m.id_student = st.id_student AND m.id_lesson = l.id_lesson
-	LEFT JOIN subject AS s ON s.id_subject = l.id_subject
-	GROUP BY st.id_student, s.id_subject
-	HAVING COUNT(mark) = 0) AS debets
-LEFT JOIN student AS std ON std.id_student = debets.id_student
-LEFT JOIN subject AS subj ON subj.id_subject = debets.id_subject
-
-
-
+EXECUTE students_with_study_debts @id_group=3
 
 --4. Дать среднюю оценку студентов по каждому предмету для тех предметов, по
 --   которым занимается не менее 35 студентов.
 
-SELECT l.id_subject, AVG(m.mark)
-FROM lesson AS l
-LEFT JOIN mark AS m ON m.id_lesson = l.id_lesson
-LEFT JOIN student AS st ON st.id_student = m.id_student
-GROUP BY l.id_subject
-HAVING COUNT(DISTINCT(st.id_student)) >= 35
-ORDER BY l.id_subject
+SELECT s.name, avg_marks.avg
+FROM(SELECT l.id_subject, AVG(m.mark) as avg
+	FROM lesson AS l
+	LEFT JOIN mark AS m ON m.id_lesson = l.id_lesson
+	LEFT JOIN student AS st ON st.id_student = m.id_student
+	GROUP BY l.id_subject
+	HAVING COUNT(DISTINCT(st.id_student)) >= 35) AS avg_marks
+LEFT JOIN subject AS s ON s.id_subject = avg_marks.id_subject
+	
+	
 
 -- 5 Дать оценки студентов специальности ВМ по всем проводимым предметам с
 --указанием группы, фамилии, предмета, даты. При отсутствии оценки заполнить
@@ -85,22 +79,12 @@ WHERE g.name = 'ПС' AND s.name = 'БД' AND l.date < '2019-05-12' AND m.mark < 5
 --ORDER BY st.name, s.name, l.date
 
 
+-- 7 Индексы
+CREATE NONCLUSTERED INDEX [subject_name_IND]
+ON subject (name)
 
+CREATE NONCLUSTERED INDEX [group_name_IND]
+ON [group] (name)
 
-
-
-
-
-
-
-
-
-
-
-
-
---index sample
-CREATE NONCLUSTERED INDEX [IX_mark_id_lesson] ON [dbo].[mark]
-(
-	[id_lesson] ASC
-)
+CREATE NONCLUSTERED INDEX [lesson_date_IND]
+ON [lesson] (date)
