93b24300af8446b33c7b6c96bf917fa7e927cd84
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-07-05 15:47:43 +0300

    7-lab ussues fix

--- lab7/SQLQuery1.sql
+++ lab7/SQLQuery1.sql
@@ -47,25 +47,23 @@ SELECT s.name, avg_marks.avg
 FROM(SELECT l.id_subject, AVG(m.mark) as avg
 	FROM lesson AS l
 	LEFT JOIN mark AS m ON m.id_lesson = l.id_lesson
-	LEFT JOIN student AS st ON st.id_student = m.id_student
+	LEFT JOIN student AS st ON st.id_group = l.id_group
 	GROUP BY l.id_subject
 	HAVING COUNT(DISTINCT(st.id_student)) >= 35) AS avg_marks
-LEFT JOIN subject AS s ON s.id_subject = avg_marks.id_subject
-	
-	
+LEFT JOIN subject AS s ON s.id_subject = avg_marks.id_subject;
+
 
 -- 5 Дать оценки студентов специальности ВМ по всем проводимым предметам с
 --указанием группы, фамилии, предмета, даты. При отсутствии оценки заполнить
 --значениями NULL поля оценки.
-SELECT g.name, st.name, s.name, m.mark, l.date--STRING_AGG(m.mark, ',') AS 'marks'
+SELECT g.name, st.name, s.name, m.mark, l.date
 FROM student AS st
-LEFT JOIN [group] AS g ON g.id_group = st.id_group
-LEFT JOIN mark AS m ON m.id_student = st.id_student
-LEFT JOIN lesson AS l ON l.id_lesson = m.id_lesson
+LEFT JOIN lesson AS l ON l.id_group = st.id_group
+LEFT JOIN mark AS m ON m.id_student = st.id_student AND m.id_lesson = l.id_lesson
 LEFT JOIN [subject] AS s ON s.id_subject = l.id_subject
+LEFT JOIN [group] AS g ON g.id_group = st.id_group
 WHERE g.name = 'ВМ'
 ORDER BY st.name, s.name, l.date;
-
 
 --6 Всем студентам специальности ПС, получившим оценки меньшие 5 по предмету
 -- БД до 12.05, повысить эти оценки на 1 балл.
